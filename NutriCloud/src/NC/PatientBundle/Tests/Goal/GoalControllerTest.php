9bd7225ed60f4d79025ee93a0710ac35d03f1408
jeremy <jeremy.maignan@epitech.eu>
2016-12-27 21:33:12 +0100

    add test goal

--- NutriCloud/src/NC/PatientBundle/Tests/Goal/GoalControllerTest.php
+++ NutriCloud/src/NC/PatientBundle/Tests/Goal/GoalControllerTest.php
@@ -8,154 +8,158 @@ use Liip\FunctionalTestBundle\Test\WebTestCase;
 class GoalControllerTest extends WebTestCase
 {
 	// GET
-    public function testAlergy1(){
+    public function testGoal1(){
         $this->loadFixtures(array("NC\\PatientBundle\\Tests\\LoadFixture"));
         // pro get alergy
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Get', '/pro/allergy/1');
-        //echo $client->getResponse()->getContent();
-        //$this->assertTrue($client->getResponse()->getContent() == '[]');
-        $this->assertTrue(1==1);
+        $client->request('Get', '/pro/goal/1');
+        $this->assertTrue($client->getResponse()->getContent() == '[]');
     }
-/*
-    public function testAlergy2(){
+
+    public function testGoal2(){
         // patient get alergy
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_patient_1', '_password' => 'test_patient_1'));
-        $client->request('Get', '/patient/allergy/1');
-		//echo $client->getResponse()->getContent();
+        $client->request('Get', '/patient/goal/1');
         $this->assertTrue($client->getResponse()->getContent() == '[]');
     }
 
     // POST 
-    public function testAlergy3(){
+    public function testGoal3(){
         // pro create alergy success
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Post', '/pro/allergy/create/1', array('name' => 'chat'));
+        $client->request('Post', '/pro/goal/create/1', array('percentage' => 10, 'text' => 'faire du sport', 'date' => 'mardi'));
 		//echo $client->getResponse()->getContent() . $client->getResponse()->getStatusCode();
         $this->assertTrue($client->getResponse()->getStatusCode() == 201);
     }
 
-    public function testAlergy4(){
+    public function testGoal4(){
         // patient create alergy success
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_patient_1', '_password' => 'test_patient_1'));
-        $client->request('Post', '/patient/allergy/create/1', array('name' => 'chat'));
+        $client->request('Post', '/patient/goal/create/1', array('percentage' => 10, 'text' => 'faire du sport', 'date' => 'mardi'));
 		//echo $client->getResponse()->getContent() . $client->getResponse()->getStatusCode();
         $this->assertTrue($client->getResponse()->getStatusCode() == 201);
     }
-
-    public function testAlergy5(){
+    public function testGoal5(){
         // pro create alergy bad id
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Post', '/pro/allergy/create/10', array('name' => 'chat'));
+        $client->request('Post', '/pro/goal/create/10', array('percentage' => 10, 'text' => 'faire du sport', 'date' => 'mardi'));
         //echo $client->getResponse()->getContent();
         $this->assertTrue($client->getResponse()->getStatusCode() == 404);
     }
 
-    public function testAlergy6(){
+    public function testGoal6(){
         // pro create alergy other patient id
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Post', '/pro/allergy/create/2', array('name' => 'chat'));
+        $client->request('Post', '/pro/goal/create/2', array('percentage' => 10, 'text' => 'faire du sport', 'date' => 'mardi'));
         //echo $client->getResponse()->getContent();
         $this->assertTrue($client->getResponse()->getStatusCode() == 403);
     }
 
     // GET
-    public function testAlergy7(){
+    public function testGoal7(){
         // pro get alergy success
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Get', '/pro/allergy/1');
+        $client->request('Get', '/pro/goal/1');
         //echo $client->getResponse()->getContent();
-        $this->assertTrue($client->getResponse()->getContent() == '[{"id": "1","name": "chat"},{"id": "2","name": "chat"}]');
+        $this->assertTrue(
+            strpos($client->getResponse()->getContent(), '"id": "1","goal": "faire du sport","percentage": "10","date": "mardi"') !== false &&
+            strpos($client->getResponse()->getContent(), '"id": "2","goal": "faire du sport","percentage": "10","date": "mardi"') !== false);
     }
 
-    public function testAlergy8(){
+    public function testGoal8(){
         // patient get alergy success
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_patient_1', '_password' => 'test_patient_1'));
-        $client->request('Get', '/patient/allergy/1');
+        $client->request('Get', '/patient/goal/1');
 		//echo $client->getResponse()->getContent();
-        $this->assertTrue($client->getResponse()->getContent() == '[{"id": "1","name": "chat"},{"id": "2","name": "chat"}]');
+        $this->assertTrue(
+            strpos($client->getResponse()->getContent(), '"id": "1","goal": "faire du sport","percentage": "10","date": "mardi"') !== false &&
+            strpos($client->getResponse()->getContent(), '"id": "2","goal": "faire du sport","percentage": "10","date": "mardi"') !== false);
     }
 
 	//PUT 
-	public function testAlergy9(){
+	public function testGoal9(){
         // pro update alergy
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Put', '/pro/allergy/update/1', array('name' => 'chien'));
+        $client->request('Put', '/pro/goal/update/1', array('percentage' => 10, 'text' => 'faire du sport', 'date' => 'lundi'));
         $this->assertTrue($client->getResponse()->getStatusCode() == 200);
     }
 
-    public function testAlergy10(){
+    public function testGoal10(){
         // patient update alergy
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_patient_1', '_password' => 'test_patient_1'));
-        $client->request('Put', '/patient/allergy/update/2', array('name' => 'chien'));
+        $client->request('Put', '/patient/goal/update/2', array('percentage' => 10, 'text' => 'faire du sport', 'date' => 'lundi'));
 		//echo $client->getResponse()->getContent() . $client->getResponse()->getStatusCode();
         $this->assertTrue($client->getResponse()->getStatusCode() == 200);
     }
 
     // GET
-    public function testAlergy11(){
+    public function testGoal11(){
         // pro get alergy success
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Get', '/pro/allergy/1');
+        $client->request('Get', '/pro/goal/1');
         //echo $client->getResponse()->getContent();
-        $this->assertTrue($client->getResponse()->getContent() == '[{"id": "1","name": "chien"},{"id": "2","name": "chien"}]');
+        $this->assertTrue(
+            strpos($client->getResponse()->getContent(), '"id": "1","goal": "faire du sport","percentage": "10","date": "lundi"') !== false &&
+            strpos($client->getResponse()->getContent(), '"id": "2","goal": "faire du sport","percentage": "10","date": "lundi"') !== false);
     }
 
-    public function testAlergy12(){
+    public function testGoal12(){
         // patient get alergy success
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_patient_1', '_password' => 'test_patient_1'));
-        $client->request('Get', '/patient/allergy/1');
+        $client->request('Get', '/patient/goal/1');
 		//echo $client->getResponse()->getContent();
-        $this->assertTrue($client->getResponse()->getContent() == '[{"id": "1","name": "chien"},{"id": "2","name": "chien"}]');
+        $this->assertTrue(
+            strpos($client->getResponse()->getContent(), '"id": "1","goal": "faire du sport","percentage": "10","date": "lundi"') !== false &&
+            strpos($client->getResponse()->getContent(), '"id": "2","goal": "faire du sport","percentage": "10","date": "lundi"') !== false);
     }
 
 	//DELETE 
-	public function testAlergy13(){
+	public function testGoal13(){
         // pro delete alergy
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Delete', '/pro/allergy/delete/1', array('name' => 'chien'));
+        $client->request('Delete', '/pro/goal/delete/1');
         $this->assertTrue($client->getResponse()->getStatusCode() == 200);
     }
 
-    public function testAlergy14(){
+    public function testGoal14(){
         // patient delete alergy
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_patient_1', '_password' => 'test_patient_1'));
-        $client->request('Delete', '/patient/allergy/delete/2', array('name' => 'chien'));
+        $client->request('Delete', '/patient/goal/delete/2');
 		//echo $client->getResponse()->getContent() . $client->getResponse()->getStatusCode();
         $this->assertTrue($client->getResponse()->getStatusCode() == 200);
     }
 
 	// GET
-    public function testAlergy15(){
+    public function testGoal15(){
         // pro get alergy
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_pro_1', '_password' => 'test_pro_1'));
-        $client->request('Get', '/pro/allergy/1');
+        $client->request('Get', '/pro/goal/1');
         //echo $client->getResponse()->getContent();
         $this->assertTrue($client->getResponse()->getContent() == '[]');
     }
 
-    public function testAlergy16(){
+    public function testGoal16(){
         // patient get alergy
         $client = static::createClient();
         $client->request('Post', '/login_check', array('_username' => 'test_patient_1', '_password' => 'test_patient_1'));
-        $client->request('Get', '/patient/allergy/1');
+        $client->request('Get', '/patient/goal/1');
 		//echo $client->getResponse()->getContent();
         $this->assertTrue($client->getResponse()->getContent() == '[]');
     }
-*/
+
 }
